daedd7861bbbb00f9e4ddd43db00a4e1fd3ef590
Crystal Su <crystals@ca.ibm.com>
2017-02-21 16:43:53 -0500

    test 15

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert'),
     vows = require('vows'),
     test1 = require('../');
 
-vows.describe('serious-calculations').addBatch({
+vows.describe('test').addBatch({
   'When performing serious calculations': {
     topic: test1.performSeriousCalculations(4),
     'result should be valid': function (result) {
