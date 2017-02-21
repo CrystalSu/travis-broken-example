2313af7bb5b1824cd65966f45d0c607e13f10cc6
Crystal Su <crystals@ca.ibm.com>
2017-02-21 16:20:23 -0500

    test 5

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -1,6 +1,6 @@
 var assert = require('assert'),
     vows = require('vows'),
-    test1 = require('test1');
+    test1 = require('../');
 
 vows.describe('serious-calculations').addBatch({
   'When performing serious calculations': {
