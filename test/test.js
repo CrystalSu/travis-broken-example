6f5b33a75a01cb383f133aafb31b5aecd39c838c
Crystal Su <crystals@ca.ibm.com>
2017-02-22 10:13:44 -0500

    test1013

--- test/test.js
+++ test/test.js
@@ -11,11 +11,20 @@ vows.describe('test').addBatch({
       assert.equal(result, 5);
     }
   },
+  /*
   'When performing test2': {
 	    topic: test2.test(3),
 	    'test is expected to fail': function (result) {
 	      assert.isNumber(result);
 	      assert.equal(result, 9); // expected result is 8
+	    }
+	  },
+  */
+  'When performing test2': {
+	    topic: test2.test(3),
+	    'test is expected to pass': function (result) {
+	      assert.isNumber(result);
+	      assert.equal(result, 8);
 	    }
 	  }
 }).export(module);
