2212db16cd57a5a81a8fd8d5aa85c43aecd8b2b3
Crystal Su <crystals@ca.ibm.com>
2017-02-22 09:24:32 -0500

    test 17

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert'),
     vows = require('vows'),
-    test1 = require('test1'),
+    test1 = require('./test1'),
     lib = require('../');
 
 vows.describe('test').addBatch({
