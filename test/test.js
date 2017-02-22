1a3f758e2f9a01ec496d16526a3176e40ac912a6
Crystal Su <crystals@ca.ibm.com>
2017-02-22 09:19:53 -0500

    test 16

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 var assert = require('assert'),
     vows = require('vows'),
-    test1 = require('../');
+    test1 = require('test1'),
+    lib = require('../');
 
 vows.describe('test').addBatch({
   'When performing serious calculations': {
-    topic: test1.performSeriousCalculations(4),
+    topic: lib.performSeriousCalculations(4),
     'result should be valid': function (result) {
       assert.isNumber(result);
       assert.equal(result, 8);
