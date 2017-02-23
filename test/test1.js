9915f90be2fd5c458a7110eb3c21c10bdc65f0aa
Crystal Su <crystals@ca.ibm.com>
2017-02-23 12:35:31 -0500

    test1235

--- test/test1.js
+++ test/test1.js
@@ -1,9 +1,7 @@
-var component1 = require('../lib/component1');
-
 exports.test = function (x) {
   //
   // call the component to be tested
   //
-  return 1 + component1.doubleIt(x);
+  return 1 + x;
 };
 
