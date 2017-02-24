29457a7840e233fec2df042b9227595bee9aada0
Crystal Su <crystals@ca.ibm.com>
2017-02-24 14:24:30 -0500

    test 1424

--- test/test.js
+++ test/test.js
@@ -1,14 +1,31 @@
 var assert = require('assert'),
     vows = require('vows'),
-    seriousCalculations = require('../lib/serious-calculations');
+    test1 = require('./test1'),
+    test2 = require('./test2');
 
-vows.describe('serious-calculations').addBatch({
-  'When performing serious calculations': {
-    topic: seriousCalculations.performSeriousCalculations(4),
-    'result should be valid': function (result) {
+vows.describe('tests').addBatch({
+  'When performing test1': {
+    topic: test1.test(2),
+    'test is expected to pass': function (result) {
       assert.isNumber(result);
-      assert.equal(result, 12);
+      assert.equal(result, 5);
     }
-  }
+  },
+  /*
+  'When performing test2': {
+	    topic: test2.test(3),
+	    'test is expected to fail': function (result) {
+	      assert.isNumber(result);
+	      assert.equal(result, 9); // expected result is 8
+	    }
+	  },
+  */
+  'When performing test2': {
+	    topic: test2.test(3),
+	    'test is expected to pass': function (result) {
+	      assert.isNumber(result);
+	      assert.equal(result, 8);
+	    }
+	  }
 }).export(module);
 
